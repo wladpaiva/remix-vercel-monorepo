ccbd89a4f5ddaaa4ae4dbe0067f33588664bf6ea
Wlad Paiva <desenvolvedormg@gmail.com>
2022-06-02 17:05:31 -0300

    submodule

--- packages/remix-app/app/routes/index.tsx
+++ packages/remix-app/app/routes/index.tsx
@@ -1,5 +1,8 @@
 import newHelper from "framework";
+import bcrypt from "bcryptjs";
 
 export default function Index() {
+  bcrypt.hash("123", 10);
+
   return <h1>printed from framework: {newHelper()}</h1>;
 }
